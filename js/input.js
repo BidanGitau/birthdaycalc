3ee43fc49651d31958e8d368addda64bcab907c9
BidanGitau <bydangitau@gmail.com>
2022-03-06 19:36:37 +0300

    added arrays

--- js/input.js
+++ js/input.js
@@ -1,36 +1,3 @@
-// function naming() {
-//     let gender = male;
-//     let males = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'kwame']
-
-//     let females = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-
-//     let Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-//     var today = new Date();
-//     var dd = String(today.getDate()).padStart(2, '0');
-//     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-//     var yyyy = today.getFullYear();
-//     today = mm + '/' + dd + '/' + yyyy;
-//     console.log(today)
-
-//     if (gender) {
-//         alert(
-//             `Your AKan name is ${males[day]} You were born on a ${daysOfWeek[day]}`
-//         );
-
-//     } else if (genderFemale) {
-//         alert(
-//             `Your AKan name is ${females[day]} You were born on a ${daysOfWeek[day]}`
-//         );
-
-//     } else {
-//         alert("Please fill  in the whole form to get back your akan name");
-//     }
-// }
-
-let dBirth = document.getElementById('date').value;
-
-
-
-
-console.log(dBirth);
+let females = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+let males = ['Akosua', 'Adowa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+let Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
