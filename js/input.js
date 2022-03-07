13186c70072d1218f4a2b8fe9e3dd7be94d580cb
BidanGitau <bydangitau@gmail.com>
2022-03-07 06:09:50 +0300

    button fix js

--- js/input.js
+++ js/input.js
@@ -1,17 +1,3 @@
-let females = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-let males = ['Akosua', 'Adowa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-let Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let checkmale = document.getElementById("male").checked;
-let checkfemale = document.getElementById("female").checked;
-let day = document.getElementById('dbirth').value;
-let dbirth = new Date(day);
-let dDay = dbirth.getDay();
-let paragrah = document.getElementById('name');
-let intro = document.getElementById('intro');
-console.log(dDay)
-console.log(checkfemale)
-console.log(checkmale)
-
 let form = document.getElementById('submit');
 
 form.addEventListener('click', (event) => {
@@ -21,6 +7,19 @@ form.addEventListener('click', (event) => {
 });
 
 function naming() {
+    let females = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    let males = ['Akosua', 'Adowa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    let Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let checkmale = document.getElementById("male").checked;
+    let checkfemale = document.getElementById("female").checked;
+    let day = document.getElementById('dbirth').value;
+    let dbirth = new Date(day);
+    let dDay = dbirth.getDay();
+    let paragrah = document.getElementById('name');
+    let intro = document.getElementById('intro');
+    console.log(dDay)
+    console.log(checkfemale)
+    console.log(checkmale)
     if (checkmale) {
         paragrah.innerText = (`your name is ${males[dDay]} and you were born on a ${Days[dDay]}`);
     } else {
